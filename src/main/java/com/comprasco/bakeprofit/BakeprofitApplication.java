b05fcba54a70d6045d020b08499b6aaae94f1f42
kstrodeveloper <aynerjosec@gmail.com>
2026-08-17 16:01:42 -0500

    Initialize project with Maven wrapper, configuration files, and basic Spring Boot application structure

---
+++ src/main/java/com/comprasco/bakeprofit/BakeprofitApplication.java
@@ -0,0 +1,13 @@
+package com.comprasco.bakeprofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeprofitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeprofitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comprasco/bakeprofit/BakeprofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comprasco.bakeprofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeprofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
