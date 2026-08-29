c6a32108a171a35df39eadc07d1b98d26e3d3287
Abisael Freyle <afreylea@unicesar.edu.co>
2026-08-29 04:04:45 -0500

    feat: crear repositorio con funciones CRUD para Category

---
+++ backend/src/main/java/com/comprasco/bakeprofit/repository/CategoryRepository.java
@@ -0,0 +1,17 @@
+package com.comprasco.bakeprofit.repository;
+
+import com.comprasco.bakeprofit.entity.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    // Buscar por nombre (coincidencia parcial, sin distinguir mayúsculas/minúsculas)
+    List<Category> findByNameContainingIgnoreCase (String name);
+
+    boolean existsByNameIgnoreCase(String name);
+
+    boolean existsByNameIgnoreCaseAndIdNot(String name, Long id);
+}
