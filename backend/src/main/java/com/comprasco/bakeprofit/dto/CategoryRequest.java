10195090113af969835c5458514879fa0623739a
Abisael Freyle <afreylea@unicesar.edu.co>
2026-08-30 17:07:15 -0500

    feat(dto): agregar anotación @Schema en DTOs Request de Category, Store y Product

--- backend/src/main/java/com/comprasco/bakeprofit/dto/CategoryRequest.java
+++ backend/src/main/java/com/comprasco/bakeprofit/dto/CategoryRequest.java
@@ -3,6 +3,7 @@ package com.comprasco.bakeprofit.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record CategoryRequest(
+    @Schema(description = "Nombre a asignar a la categoria")
     @NotBlank(message = "El nombre de la categoria es obligatorio")
     String name
 ) {}
